dd119cd4aec10d2e0d9329641aa1f37c69d9079e
Ian Bull <irbull@eclipsesource.com>
2017-06-21 16:14:07 -0700

    Generate a table of known sqr roots

--- src/MathFunctions/mysqrt.cxx
+++ src/MathFunctions/mysqrt.cxx
@@ -1,8 +1,12 @@
 #include "TutorialConfig.h"
 
 #include <math.h>
+#include "Table.h"
 
 double mysqrt(double x) {
+  if ( x < 10 ) {
+    return sqrtTable[static_cast<int>(x)];
+  }
 #if defined (HAVE_LOG) && defined (HAVE_EXP)
   return exp(log(x)*0.5);
 # else
