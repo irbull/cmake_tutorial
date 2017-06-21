58e00f399cc920013442c584f80518d61544f317
Ian Bull <irbull@eclipsesource.com>
2017-06-21 12:24:57 -0700

    Add system introspection
    Check that exp and log exist, and if they do, use them for
    computing the square root.

--- src/MathFunctions/mysqrt.cxx
+++ src/MathFunctions/mysqrt.cxx
@@ -1,3 +1,11 @@
+#include "TutorialConfig.h"
+
+#include <math.h>
+
 double mysqrt(double x) {
+#if defined (HAVE_LOG) && defined (HAVE_EXP)
+  return exp(log(x)*0.5);
+# else
   return 7.1;
+# endif
 }
